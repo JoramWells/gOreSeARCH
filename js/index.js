648a4bb1e270a0ca810e275cf6c24d7a513732d1
bramuel <jorammanoah1@gmail.com>
2021-07-16 12:27:10 +0300

    Added navbar active

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
+const text = "wtf";
+const speed = 50;
+
 var form = document.getElementById("form");
-function handleSubmit(e){
-    e.preventDefault()
+function handleSubmit(e) {
+  e.preventDefault();
 }
-form.addEventListener('submit',handleSubmit);
+form.addEventListener("submit", handleSubmit);
+
+const typeWriter = () => {
+  if (text.length) {
+    document.getElementById("type").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+};
+
+const header = document.querySelector(".navbar");
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >600) {
+  header.classList.add("active");
+} else {
+  header.classList.remove("active");
+}
+  console.log(top);
+};
+
